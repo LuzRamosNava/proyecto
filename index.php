131ac29d14a982bfef27cea76a78539a7f14a2c9
LuzRamosNava <114432504+LuzRamosNava@users.noreply.github.com>
2022-10-05 13:53:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+	<script src="https://kit.fontawesome.com/0ee54e0a21.js" crossorigin="anonymous"></script>
+		<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	<title>CV Personal</title> 
+</head>
+<body>
+	
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+
+
+		<a class="navbar-brand" style="background-color:#338b85" href="#">Sobre mí</a>
+		<div class="navbar-nav mr-auto" id="navbarNav">
+			<ul class="navbar-nav" >
+				<li class="nav-item">
+				<a class="nav-link active" aria-current="page"  href="#estudios">Estudios</a> 
+				</li>
+
+				<li class="nav-item">
+				<a class="nav-link" href="#portafolio">Portafolio</a>
+				</li>
+
+				<li class="nav-item">
+				<a class="nav-link" href="#intereses">Intereses</a>
+				</li>
+
+				<li class="nav-item">
+				<a class="nav-link" href="#contacto">Contacto</a>
+				</li>
+			</ul>
+		</div>
+		</div>
+</nav>  <br>
+
+
+<div class="container" style="padding-top: 50px">
+	<div class="row align-items-start">
+		<div class="col-6">
+			<h1 align="center" class="card-subtitle mb-2 text-muted">Andrea de la Luz Ramos Nava </h1>
+
+
+
+				<p style="text-align: justify;"> <i> Mantente alejado de aquellas personas que tratan de menospreciar tus ambiciones. Las personas pequeñas siempre lo hacen, pero los verdaderamente grandes hacen sentirte que tú también puedes ser grande – Mark Twain </i></p>
+			</div>
+
+<div class="col-6 col" >
+	<img src="imagenes/luznava.jpg" style="max-width: 540px;" class="img-fluid rounded-start" alt="...">
+</div>
+	</div>  <br>
+
+<div id="estudios" class="container-fluid" style="background-color:#d5ffff; color:white; padding-top:60px">
+	<div class="row text-center">
+		<h1 class="display-1" style="color:black;">ESTUDIOS</h1> <br> <br>
+
+
+<div class="container">
+<div class="row align-items-start">
+<div class="col">
+
+<table class="table">
+	<thead>
+	    <tr>
+		    <th scope="col" style="background-color: #338b85";></th>
+		    <th scope="col" style="background-color: #338b85";>Institución Educativa</th>
+	    </tr>
+	</thead>
+								
+<tbody>
+	<tr align="left">
+		<th scope="row">1</th>
+		    <td>Escuela Secundaria No.20 </td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">2</th>
+		<td>Centro de Estudios Tecnológicos industrial y de servicios No. 104 Aquiles Serdán Alatriste.</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">3</th>
+		<td>Benemérito Conservatorio de Música del Estado de Puebla.</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">4</th>
+	    <td colspan="2">Universidad Tecnológica de Huejotzingo</td>
+	</tr>
+</tbody>
+
+	<thead>
+	    <tr>
+		    <th scope="col" style="background-color: #338b85";></th>
+		    <th scope="col" style="background-color: #338b85";>Cursos</th>
+	    </tr>
+	</thead>
+
+<tbody>
+	<tr align="left">
+		<th scope="row">1</th>
+			<td>JavaScript</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">2</th>
+			<td>Usabilidad</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">3</th>
+		    <td colspan="2">HTML 5 y CSS3</td>
+	</tr>
+
+    <tr align="left">
+		<th scope="row">4</th>
+			<td colspan="2">Scrum Fundamentals Certified</td>
+	</tr>
+
+    <tr align="left">
+		<th scope="row">5</th>
+		    <td colspan="2">Título en Técnico en Programación</td>
+	</tr>
+<br>
+
+</tbody>
+<br>
+
+	<thead>
+		<tr>
+			<th scope="col" style="background-color: #338b85;"></th>
+				<th scope="col" style="background-color: #338b85;">Conocimientos y Habilidades</th>
+		</tr>
+	</thead>
+
+<tbody>
+	<tr align="left">
+		<th scope="row">1</th>
+			<td>Concierto de saxofon y canto. </td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">2</th>
+			<td>Lingüística: Italiano</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">3</th>
+		    <td colspan="2">Logros academicos.</td>
+	</tr>
+
+    <tr align="left">
+		<th scope="row">4</th>
+			<td colspan="2">Trabajo en equipo.</td>
+	</tr>
+
+	<tr align="left">
+		<th scope="row">5</th>
+			<td colspan="2">Actividades extracurriculares</td>
+	</tr>
+
+</tbody>
+
+</table>
+
+</div>
+
+    <div class="col">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+			<div class="carousel-indicators">
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
+
+<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="imagenes/concierto.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block">
+				</div>
+		</div>
+
+
+
+	<div class="carousel-item">
+		<img src="imagenes/titulo.jpg" class="d-block w-100" alt="...">
+			<div class="carousel-caption d-none d-md-block">	
+			</div>
+	</div>
+
+
+	<div class="carousel-item">
+		<img src="imagenes/scrum.jpg" class="d-block w-100" alt="...">
+			<div class="carousel-caption d-none d-md-block">
+			</div>
+	</div>
+</div>
+								
+	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Previous</span>
+	</button>
+								
+	<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Next</span>
+	</button>
+	</div>
+		</div>
+			</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+
+
+	<div id="portafolio" class="container" style="background-color:#5dc1b9; color:black; padding-top:50px">
+		<div style="text-align: justify;">
+				<h1 class="display-1" style="color:black;">PORTAFOLIO</h1> <br> <br>
+
+  <div class="row align-items-start">
+
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/topologia.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Primer cuatrimestre</h5>
+        <p class="card-text"> Identificar y desarrollar las habilidades tecnológicas adquiridas a través de diversos escenarios de aplicación de las TIC’s</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/fora.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Segundo cuatrimestre</h5>
+        <p class="card-text"> Administrar datos a traves de diferentes técnicas y tecnologías para asegurar la
+integridad de la información de las organizaciones. </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+
+<br>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagenes/servicio.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/equipofora.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/equipo1.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
+
+<br>
+
+<div id="intereses" class="container-fluid" style="background-color:#9ce0db; color:black; padding-top: 40px">
+			<div class="row text-center">
+				<h1 class="display-1" style="color:black;"> INTERESES</h1>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card" >
+
+<i class="fa-solid fa-saxophone"></i>
+
+      <div class="card-body">
+        <h5 class="card-title">Música</h5>
+        <p class="card-text"> La música me apasiona porque sin la música la vida sería una equivocación. Además desde las 15 años práctico instrumentos como es el saxofón, la guitarra y flauta transversal.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+
+<i class="fa-solid fa-gamepad-modern"></i>
+
+      <div class="card-body">
+        <h5 class="card-title">Videojugos</h5>
+        <p class="card-text"> Me gusta de vez en cuando jugar videojuegos con mis amistades y hermanos.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+
+<i class="fa-solid fa-book-open-cover"></i>
+
+      <div class="card-body">
+        <h5 class="card-title">Lectura</h5>
+        <p class="card-text">Me gusta leer y adentrarme mas allá de lo que la imaginación puede ser capaz. </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<br>
+
+			<div id="contacto" class="container-fluid" style="background-color:#005954; color:white;">
+				<div class="row text-center">
+					<h1 class="display-1"> CONTACTO</h1>
+
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-6 text-center">
+						
+						<h3>REDES SOCIALES</h3>
+						<H1>
+					<a href=https://www.facebook.com/andrea.ramosn.7><ion-icon name="logo-facebook"></ion-icon></a> 
+					<a href=https://www.instagram.com/luuuuz_nava/?fbclid=IwAR1SwKxfJ_FIVuOPU8j7UNcx_j40DcIq28E-9xFLjYwXeLmaapnqBiO0VMg><ion-icon name="logo-instagram"></ion-icon></a>
+					<a href="https://twitter.com/?lang=es"><ion-icon name="logo-twitter"></ion-icon></a><br>	
+					
+					 <br>
+
+					</div>
+					<div class="col-md-6 text-center">
+						
+						<span class="material-icons">location_on </span> San Miguel Xoxtla, Puebla. <br>
+						<span class="material-icons">phone </span> 2211480568 <br>
+						<span class="material-icons">mail </span> 3521110177@uth.edu.mx 
+						
+						
+					</div>
+				</H1>
+			</div>
+		</div>
+		<br>
+					<p align="center">Derechos Reservados 2022</p>
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+</body>
+</html>
